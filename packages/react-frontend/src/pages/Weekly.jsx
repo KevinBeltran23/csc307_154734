133106cb3eb0837e8ad866997ca120adf854bcbf
MaxSchemenauer <113937793+MaxSchemenauer@users.noreply.github.com>
2024-05-12 23:59:11 -0700

    all parts, jumbled

--- packages/react-frontend/src/pages/Weekly.jsx
+++ packages/react-frontend/src/pages/Weekly.jsx
@@ -1,77 +1,128 @@
-// src/pages/Weekly.jsx
-import React from "react";
+import React from 'react';
+import '../components/Weekly.css';
 
-const Weekly = () => {
+function MyComponent() {
   return (
-    <div className="weekly position-relative">
-      {/* Main Box */}
-      <div className="main-box"></div>
-
+    <div>
+      {/* imageedit_6_4992620932 1 */}
+      <div className="imageedit_6_4992620932"></div>
+      
+      {/* Weekly */}
+      <div className="weekly"></div>
+      
       {/* Expand */}
       <div className="expand"></div>
-
+      
       {/* Days */}
       <div className="days">
-        {/* SUN */}
-        <div>SUN</div>
-        {/* SAT */}
-        <div>SAT</div>
-        {/* FRI */}
-        <div>FRI</div>
-        {/* THU */}
-        <div>THU</div>
-        {/* WED */}
-        <div>WED</div>
-        {/* TUE */}
-        <div>TUE</div>
-        {/* MON */}
-        <div>MON</div>
+        <div className="sun"></div>
+        <div className="mon"></div>
+        <div className="tue"></div>
+        <div className="wed"></div>
+        <div className="thu"></div>
+        <div className="fri"></div>
+        <div className="sat"></div>
       </div>
-
+      
       {/* TopRightNavBar */}
       <div className="top-right-nav-bar">
-        {/* Line 11 */}
         <div className="line-11"></div>
-        {/* Ellipse 2 */}
         <div className="ellipse-2"></div>
-      </div>
-
-      {/* Change View */}
-      <div className="change-view">
-        {/* Weekly View */}
-        <div className="weekly-view">Weekly View</div>
-        {/* Ellipse 5 */}
-        <div className="ellipse-5"></div>
-      </div>
-
-      {/* Create */}
-      <div className="create">
-        {/* Frame 6 */}
-        <div className="frame-6">
-          {/* Create */}
-          <div className="create-text">Create</div>
-          {/* Polygon 1 */}
-          <div className="polygon-1"></div>
+        <div className="change-view">
+          <div className="weekly-view">Weekly View</div>
+          <div className="ellipse-5"></div>
         </div>
       </div>
-
-      {/* Frame 10 */}
-      <div className="frame-10"></div>
-
-      {/* Change View 2 */}
-      <div className="change-view-2">
-        {/* Monthly View */}
-        <div className="monthly-view">Monthly View</div>
-        {/* Ellipse 5 */}
-        <div className="ellipse-5-2"></div>
-        {/* Polygon 2 */}
-        <div className="polygon-2"></div>
+      
+      {/* TopLeftNavBar */}
+      <div className="top-left-nav-bar">
+        <div className="polygon-3"></div>
+        <div className="polygon-4"></div>
+        <div className="week-1">Week 1</div>
+        <div className="go-to-today">
+          <div className="today">Today</div>
+          <div className="polygon-5"></div>
+        </div>
       </div>
-
-      {/* Imageedit 6 4992620932 */}
-      <div className="imageedit-6-4992620932"></div>
+      
+      {/* CalendarGrid */}
+      <div className="calendar-grid">
+        <div className="rectangle-2"></div>
+        <div className="line line-1"></div>
+        <div className="line line-2"></div>
+        <div className="line line-3"></div>
+        <div className="line line-4"></div>
+        <div className="line line-5"></div>
+        <div className="line line-6"></div>
+        <div className="rectangle-41"></div>
+        <div className="rectangle-42"></div>
+        <div className="rectangle-43"></div>
+        <div className="rectangle-44"></div>
+        <div className="rectangle-45"></div>
+        <div className="rectangle-46"></div>
+        <div className="rectangle-47"></div>
+        <div className="rectangle-77"></div>
+        <div className="rectangle-87"></div>
+        <div className="rectangle-78"></div>
+        <div className="rectangle-82"></div>
+        <div className="rectangle-81"></div>
+        <div className="rectangle-79"></div>
+        <div className="rectangle-86"></div>
+        <div className="rectangle-80"></div>
+      </div>
+      
+      {/* Download */}
+      <div className="download">
+        <div className="rectangle-88"></div>
+        <div className="download-icon">
+          <div className="vector-6"></div>
+          <div className="vector-7"></div>
+        </div>
+      </div>
+      
+      {/* 6:22 PM */}
+      <div className="time">6:22 PM</div>
+      
+      {/* Sidebar */}
+      <div className="sidebar">
+        <div className="rectangle-1"></div>
+        
+        {/* Calendars */}
+        <div className="calendars">
+          <div className="frame-18">
+            <div className="calendars-heading">Calendars</div>
+            <div className="polygon-6"></div>
+          </div>
+        </div>
+        
+        {/* ToDo */}
+        <div className="todo">
+          <div className="frame-22">
+            <div className="todo-item">To Do</div>
+            <div className="polygon-6"></div>
+          </div>
+        </div>
+        
+        {/* Time slots */}
+        <div className="time-slot time-slot-8am"></div>
+        <div className="time-slot time-slot-9am"></div>
+        <div className="time-slot time-slot-10am"></div>
+        <div className="time-slot time-slot-11am"></div>
+        <div className="time-slot time-slot-12pm"></div>
+        <div className="time-slot time-slot-1pm"></div>
+        <div className="time-slot time-slot-2pm"></div>
+        <div className="time-slot time-slot-3pm"></div>
+        <div className="time-slot time-slot-4pm"></div>
+        <div className="time-slot time-slot-5pm"></div>
+        <div className="time-slot time-slot-6pm"></div>
+        <div className="time-slot time-slot-7pm"></div>
+        <div className="time-slot time-slot-8pm"></div>
+        
+        {/* Output Image */}
+        <div className="output-image"></div>
+      </div>
     </div>
   );
-};
+}
 
-export default Weekly;
+export default MyComponent;
