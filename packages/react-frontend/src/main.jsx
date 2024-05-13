dd7726097b8062d1ef914b18771bc6160f802acd
klseverson <113937747+klseverson@users.noreply.github.com>
2024-05-12 21:22:57 -0700

    broken todo and pages

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -2,13 +2,27 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 import Login from "./pages/Login"; 
-import "./components/Login.css";
+// import ToDo from "./pages/ToDo";
+import BrowserRouter from 'react-router-dom';
+// import "./components/Login.css";
 
-// Create the container
+ // Create the container
 const container = document.getElementById("root");
 
 // Create a root
 const root = ReactDOMClient.createRoot(container);
 
 // Initial render: Render the MyApp component to the Root
-root.render(<Login />);
+root.render(
+    <BrowserRouter>
+    <div className="main">
+      <Routes>
+        <Route path="/" element={<Login/>} />
+      </Routes>
+    </div>
+  </BrowserRouter>
+);
+
+
+//root.render(<ToDo />);
+
