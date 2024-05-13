c4f91aac4ccce621070fb7a4d364959b1c6069e7
MaxSchemenauer <113937793+MaxSchemenauer@users.noreply.github.com>
2024-05-12 23:53:13 -0700

    jumbled and missing

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,37 +1,11 @@
-// src/main.jsx
-/* import React from "react";
-import ReactDOMClient from "react-dom/client";
-import Login from "./pages/Login"; 
-// import ToDo from "./pages/ToDo";
-import BrowserRouter from 'react-router-dom';
-// import "./components/Login.css";
-
- // Create the container
-const container = document.getElementById("root");
-
-// Create a root
-const root = ReactDOMClient.createRoot(container);
-
-// Initial render: Render the MyApp component to the Root
-root.render(
-    <BrowserRouter>
-    <div className="main">
-      <Routes>
-        <Route path="/" element={<Login/>} />
-      </Routes>
-    </div>
-  </BrowserRouter>
-);
-*/
-
-//root.render(<ToDo />);
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import Login from "./pages/Login"; 
 import "./components/Login.css";
 import ToDo from "./pages/ToDo";
+import Weekly from "./pages/Weekly"; // Make sure the import is correct
+import "./components/Weekly.css";
 
 // Create the container
 const container = document.getElementById("root");
@@ -46,6 +20,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/ToDo" element={<ToDo/>} />
+        <Route path="/Weekly" element={<Weekly/>} /> // Ensure the route is correctly set up
       </Routes>
     </div>
   </Router>
--- packages/react-frontend/src/pages/Weekly.jsx
+++ packages/react-frontend/src/pages/Weekly.jsx
@@ -3,36 +3,73 @@ import React from "react";
 
 const Weekly = () => {
   return (
-    <div className="position-relative">
+    <div className="weekly position-relative">
       {/* Main Box */}
       <div className="main-box"></div>
 
-      {/* Username Box */}
-      <div className="username-box"></div>
+      {/* Expand */}
+      <div className="expand"></div>
 
-      {/* Password Box */}
-      <div className="password-box"></div>
-
-      {/* Login Box */}
-      <div className="login-box"></div>
-
-      {/* Gold Box */}
-      <div className="gold-box"></div>
-      
-      {/* Text elements */}
-      <div className="text-elements">
-        {/* #154734 */}
-        <div className="poly-planner">Poly Planner</div>
-
-        {/* Username */}
-        <div className="username">Username</div>
-
-        {/* Password */}
-        <div className="password">Password</div>
-
-        {/* Login */}
-        <div className="login">Login</div>
+      {/* Days */}
+      <div className="days">
+        {/* SUN */}
+        <div>SUN</div>
+        {/* SAT */}
+        <div>SAT</div>
+        {/* FRI */}
+        <div>FRI</div>
+        {/* THU */}
+        <div>THU</div>
+        {/* WED */}
+        <div>WED</div>
+        {/* TUE */}
+        <div>TUE</div>
+        {/* MON */}
+        <div>MON</div>
       </div>
+
+      {/* TopRightNavBar */}
+      <div className="top-right-nav-bar">
+        {/* Line 11 */}
+        <div className="line-11"></div>
+        {/* Ellipse 2 */}
+        <div className="ellipse-2"></div>
+      </div>
+
+      {/* Change View */}
+      <div className="change-view">
+        {/* Weekly View */}
+        <div className="weekly-view">Weekly View</div>
+        {/* Ellipse 5 */}
+        <div className="ellipse-5"></div>
+      </div>
+
+      {/* Create */}
+      <div className="create">
+        {/* Frame 6 */}
+        <div className="frame-6">
+          {/* Create */}
+          <div className="create-text">Create</div>
+          {/* Polygon 1 */}
+          <div className="polygon-1"></div>
+        </div>
+      </div>
+
+      {/* Frame 10 */}
+      <div className="frame-10"></div>
+
+      {/* Change View 2 */}
+      <div className="change-view-2">
+        {/* Monthly View */}
+        <div className="monthly-view">Monthly View</div>
+        {/* Ellipse 5 */}
+        <div className="ellipse-5-2"></div>
+        {/* Polygon 2 */}
+        <div className="polygon-2"></div>
+      </div>
+
+      {/* Imageedit 6 4992620932 */}
+      <div className="imageedit-6-4992620932"></div>
     </div>
   );
 };
