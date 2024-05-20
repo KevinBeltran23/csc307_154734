ee99d33dc3aba4942f814e11b3107f9663d904a8
KevinBeltran23 <kevinbeltran2437@gmail.com>
2024-05-20 15:23:57 -0700

    visual fixes

--- packages/react-frontend/src/pages/Login.jsx
+++ packages/react-frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
+
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import "../components/Login.css";
+import "../components/Login.css"
 
 function Login(props) {
   const [creds, setCreds] = useState({
@@ -8,87 +9,74 @@ function Login(props) {
     pwd: ""
   });
 
-  const navigate = useNavigate(); // Access the history object
+  const navigate = useNavigate(); 
 
   return (
     <div className="login-position-relative">
       {/* Main Box */}
-      <div className="login-main-box"></div>
+      <div className="login-main-box">
+        {/* Gold Box */}
+        <div className="login-gold-box"></div>
 
-      {/* Username Box */}
-      <input
-        className="login-username-box"
-        type="text"
-        name="username"
-        value={creds.username}
-        onChange={handleChange}
-        style={{ fontSize: "18px" }}
-      />
-
-      {/* Password Box */}
-      <input
-        className="login-password-box"
-        type="password"
-        name="password"
-        value={creds.pwd}
-        onChange={handleChange}
-        style={{ fontSize: "18px" }}
-      />
-
-      {/* Error Message */}
-      {props.message && <div className="login-error-message">{props.message}</div>}
-
-      {/* Login Box */}
-      <button className="login-login-box" onClick={submitForm}> 
-        <p className="login-login">Login</p>
-      </button>
-
-      {/* Register Box */}
-      <button 
-        className="login-register-box" onClick={handleRegistration}> 
-        <p className="login-register">Create an Account</p>
-      </button>
-
-      {/* Gold Box */}
-      <div className="login-gold-box"></div>
-
-      {/* Text elements */}
-      <div className="login-text-elements">
         {/* Poly Planner */}
         <div className="login-poly-planner">Poly Planner</div>
 
         {/* Username */}
         <div className="login-username">Username</div>
+        <input
+          className="login-username-box"
+          type="text"
+          name="username"
+          value={creds.username}
+          onChange={handleChange}
+          style={{ fontSize: "18px" }}
+        />
 
         {/* Password */}
         <div className="login-password">Password</div>
+        <input
+          className="login-password-box"
+          type="password"
+          name="password"
+          value={creds.pwd}
+          onChange={handleChange}
+          style={{ fontSize: "18px" }}
+        />
+
+        {/* Error Message */}
+        {props.message && <div className="login-error-message">{props.message}</div>}
+
+        {/* Login Box */}
+        <button className="login-login-box" onClick={submitForm}>
+          <p className="login-login">Login</p>
+        </button>
+
+        {/* Register Button */}
+        <button className="login-register-button" onClick={handleRegister}> 
+          <p className="login-register-text">Create Account</p>
+        </button>
       </div>
     </div>
   );
 
   function handleChange(event) {
     const { name, value } = event.target;
-    switch (name) {
-      case "username":
-        setCreds({ ...creds, username: value });
-        break;
-      case "password":
-        setCreds({ ...creds, pwd: value });
-        break;
-    }
+    setCreds(prevCreds => ({ ...prevCreds, [name]: value }));
   }
 
-  function handleRegistration() {
-    navigate('/signup')
+  function handleRegister() {
+    navigate('/signup');
   }
 
   function submitForm() {
     props.handleSubmit(creds)
       .then((response) => {
-        if (response == 1) {
-            navigate('/monthly');
-        }})
+        if (response === 1) {
+          navigate('/monthly');
+        }
+      });
     setCreds({ username: "", pwd: "" });
   }
 }
+
 export default Login;
--- packages/react-frontend/src/pages/SignUp.jsx
+++ packages/react-frontend/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
+
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "../components/SignUp.css"
-
 
 function SignUp(props) {
   const [creds, setCreds] = useState({
@@ -14,73 +14,69 @@ function SignUp(props) {
   return (
     <div className="signup-position-relative">
       {/* Main Box */}
-      <div className="signup-main-box"></div>
+      <div className="signup-main-box">
+        {/* Gold Box */}
+        <div className="signup-gold-box"></div>
 
-      {/* Username Box */}
-      <input
-        className="signup-username-box"
-        type="text"
-        name="username"
-        value={creds.username}
-        onChange={handleChange}
-        style={{ fontSize: "18px" }}
-      />
-
-      {/* Password Box */}
-      <input
-        className="signup-password-box"
-        type="password"
-        name="password"
-        value={creds.pwd}
-        onChange={handleChange}
-        style={{ fontSize: "18px" }}
-      />
-
-      {/* Error Message */}
-      {props.message && <div className="signup-error-message">{props.message}</div>}
-
-      {/* Login Box */}
-      <button className="signup-login-box" onClick={submitForm}> </button>
-
-      {/* Gold Box */}
-      <div className="signup-gold-box"></div>
-      
-      {/* Text elements */}
-      <div className="signup-text-elements">
         {/* Poly Planner */}
         <div className="signup-poly-planner">Poly Planner</div>
 
         {/* Username */}
         <div className="signup-username">Username</div>
+        <input
+          className="signup-username-box"
+          type="text"
+          name="username"
+          value={creds.username}
+          onChange={handleChange}
+          style={{ fontSize: "18px" }}
+        />
 
         {/* Password */}
         <div className="signup-password">Password</div>
+        <input
+          className="signup-password-box"
+          type="password"
+          name="password"
+          value={creds.pwd}
+          onChange={handleChange}
+          style={{ fontSize: "18px" }}
+        />
 
-        {/* Login */}
-        <div className="signup-login">Sign Up</div>
+        {/* Error Message */}
+        {props.message && <div className="signup-error-message">{props.message}</div>}
+
+        {/* Sign Up Box */}
+        <button className="signup-login-box" onClick={submitForm}>
+          <p className="signup-login">Sign Up</p>
+        </button>
+
+        {/* Login Box */}
+        <button className="signup-return-button" onClick={handleReturn}>
+          <p className="signup-return-text">Return To Login</p>
+        </button>
       </div>
     </div>
   );
 
   function handleChange(event) {
     const { name, value } = event.target;
-    switch (name) {
-      case "username":
-        setCreds({ ...creds, username: value });
-        break;
-      case "password":
-        setCreds({ ...creds, pwd: value });
-        break;
-    }
+    setCreds(prevCreds => ({ ...prevCreds, [name]: value }));
+  }
+
+  function handleReturn() {
+    navigate('/');
   }
 
   function submitForm() {
     props.handleSubmit(creds)
-    .then((response) => {
-      if (response == 1) {
+      .then((response) => {
+        if (response === 1) {
           navigate('/monthly');
-      }})
+        }
+      });
     setCreds({ username: "", pwd: "" });
   }
 }
+
 export default SignUp;
