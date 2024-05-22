78fb8c027569de6027e7e9316fd998982959d7d7
madeline_park2 <madelinepark@ymail.com>
2024-05-22 09:26:29 -0700

    temp

--- packages/react-frontend/src/pages/Monthly.jsx
+++ packages/react-frontend/src/pages/Monthly.jsx
@@ -1,7 +1,8 @@
 // src/pages/Monthly.jsx
 import React, { useState } from "react";
-import Calendar from "react-calendar";
-import "../components/Monthly.css";
+/*import Calendar from "react-calendar";
+import "../components/Monthly.css"; */
+import BigCalendar from "react-big-calendar";
 
 function Monthly() {
     const [value, setValue] = useState(new Date());
