07d3a8b1735a80d9229659d94ab273abe6b3f0cb
klseverson <113937747+klseverson@users.noreply.github.com>
2024-06-03 08:25:35 -0700

    no lint frontend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,40 @@ import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReactConfig,
+  {
+    languageOptions: { globals: globals.browser }},
+    pluginJs.configs.recommended,
+    pluginReactConfig,
 ];
+
+/*import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2021,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+    },
+    overrides: [
+      {
+        files: ['src/**\/*.{js,jsx}'],
+      },
+    ],
+  },
+];
+ */
