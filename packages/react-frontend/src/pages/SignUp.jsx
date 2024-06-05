5582d01f0b79be4eadc5dbcd5d434b45737d3c00
Madeline Park <153243234+madeline-park2@users.noreply.github.com>
2024-06-04 22:59:14 -0700

    Update SignUp.jsx

--- packages/react-frontend/src/pages/SignUp.jsx
+++ packages/react-frontend/src/pages/SignUp.jsx
@@ -1,109 +1,109 @@
-
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import "../components/SignUp.css"
+import { useNavigate } from "react-router-dom";
+import "../components/SignUp.css";
 
 function SignUp(props) {
-  const [creds, setCreds] = useState({
-    username: "",
-    pwd: "",
-    confirmPwd: "" 
-  });
+    const [creds, setCreds] = useState({
+        username: "",
+        pwd: "",
+        confirmPwd: ""
+    });
 
-  const navigate = useNavigate(); 
+    const navigate = useNavigate();
 
-  return (
-    <div className="signup-position-relative">
-      {/* Main Box */}
-      <div className="signup-main-box">
-        {/* Gold Box */}
-        <div className="signup-gold-box"></div>
-
-        {/* Poly Planner */}
-        <div className="signup-poly-planner">Poly Planner</div>
-
-        {/* Username */}
-        <div className="signup-username">Username</div>
-        <input
-          className="signup-username-box"
-          type="text"
-          name="username"
-          value={creds.username}
-          onChange={handleChange}
-          style={{ fontSize: "18px" }}
-        />
-
-        {/* Password */}
-        <div className="signup-password">Password</div>
-        <input
-          className="signup-password-box"
-          type="password"
-          name="password"
-          value={creds.pwd}
-          onChange={handleChange}
-          style={{ fontSize: "18px" }}
-        />
-
-        {/* Confirm Password */}
-        <div className="signup-confirm-password">Confirm Password</div>
-        <input
-          className="signup-confirm-password-box"
-          type="password"
-          name="confirmPwd"
-          value={creds.confirmPwd}
-          onChange={handleChange}
-          style={{ fontSize: "18px" }}
-        />
-
-        {/* Error Message */}
-        {props.message && <div className="signup-error-message">{props.message}</div>}
-
-        {/* Sign Up Box */}
-        <button className="signup-login-box" onClick={submitForm}>
-          <p className="signup-login">Sign Up</p>
-        </button>
-
-        {/* Login Box */}
-        <button className="signup-return-button" onClick={handleReturn}>
-          <p className="signup-return-text">Return To Login</p>
-        </button>
-      </div>
-    </div>
-  );
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-    switch (name) {
-      case "username":
-        setCreds({ ...creds, username: value });
-        break;
-      case "password":
-        setCreds({ ...creds, pwd: value });
-        break;
-      case "confirmPwd":
-        setCreds({ ...creds, confirmPwd: value });
-        break;
-    }
-  }
-
-  function handleReturn() {
-    navigate('/');
-  }
-
-  function submitForm() {
-    if (creds.pwd !== creds.confirmPwd) {
-      props.setMessage("Signup Error: Passwords do not match");
-      return;
+    function handleReturn() {
+        navigate("/");
     }
 
-    props.handleSubmit(creds)
-      .then((response) => {
-        if (response === 1) {
-          navigate('/monthly');
+    function submitForm() {
+        if (creds.pwd !== creds.confirmPwd) {
+            props.setMessage("Signup Error: Passwords do not match");
+            return;
         }
-      });
-    setCreds({ username: "", pwd: "", confirmPwd: "" });
-  }
+
+        props.handleSubmit(creds).then((response) => {
+            if (response === true) {
+                navigate("/monthly");
+            }
+        });
+        setCreds({ username: "", pwd: "", confirmPwd: "" });
+    }
+
+    return (
+        <div className="signup-position-relative">
+            {/* Main Box */}
+            <div className="signup-main-box">
+                {/* Gold Box */}
+                <div className="signup-gold-box"></div>
+
+                {/* Poly Planner */}
+                <div className="signup-poly-planner">Poly Planner</div>
+
+                {/* Username */}
+                <div className="signup-username">Username</div>
+                <input
+                    className="signup-username-box"
+                    type="text"
+                    name="username"
+                    value={creds.username}
+                    onChange={handleChange}
+                    style={{ fontSize: "18px" }}
+                />
+
+                {/* Password */}
+                <div className="signup-password">Password</div>
+                <input
+                    className="signup-password-box"
+                    type="password"
+                    name="password"
+                    value={creds.pwd}
+                    onChange={handleChange}
+                    style={{ fontSize: "18px" }}
+                />
+
+                {/* Confirm Password */}
+                <div className="signup-confirm-password">Confirm Password</div>
+                <input
+                    className="signup-confirm-password-box"
+                    type="password"
+                    name="confirmPwd"
+                    value={creds.confirmPwd}
+                    onChange={handleChange}
+                    style={{ fontSize: "18px" }}
+                />
+
+                {/* Error Message */}
+                {props.message && (
+                    <div className="signup-error-message">{props.message}</div>
+                )}
+
+                {/* Sign Up Box */}
+                <button className="signup-login-box" onClick={submitForm}>
+                    <p className="signup-login">Sign Up</p>
+                </button>
+
+                {/* Login Box */}
+                <button className="signup-return-button" onClick={handleReturn}>
+                    <p className="signup-return-text">Return To Login</p>
+                </button>
+            </div>
+        </div>
+    );
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        switch (name) {
+            case "username":
+                setCreds({ ...creds, username: value });
+                break;
+            case "password":
+                setCreds({ ...creds, pwd: value });
+                break;
+            case "confirmPwd":
+                setCreds({ ...creds, confirmPwd: value });
+                break;
+        }
+    }
 }
 
 export default SignUp;
