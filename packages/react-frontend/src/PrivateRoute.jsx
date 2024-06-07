0750b5da6249edddc46f23a737e5b476581847b6
klseverson <113937747+klseverson@users.noreply.github.com>
2024-06-07 00:36:00 -0700

    no authentication

--- packages/react-frontend/src/PrivateRoute.jsx
+++ packages/react-frontend/src/PrivateRoute.jsx
@@ -4,11 +4,11 @@ import { Navigate } from "react-router-dom";
 function PrivateRoute({ element: Component, ...rest }) {
     const isAuthenticated = localStorage.getItem("isAuthenticated");
 
-    if (isAuthenticated) {
+    /*if (isAuthenticated) {
         return <Component {...rest} />;
     } else {
         return <Navigate to="/" />;
-    }
+    }*/
 }
 
 export default PrivateRoute;
