683f459816aa0c2ee4f3c83fceb5c257f012807c
MaxSchemenauer <113937793+MaxSchemenauer@users.noreply.github.com>
2024-06-07 01:13:56 -0700

    login resizes

--- packages/react-frontend/src/pages/Login.jsx
+++ packages/react-frontend/src/pages/Login.jsx
@@ -37,55 +37,52 @@ function Login(props) {
     }
 
     return (
-        <div className="login-position-relative">
-            {/* Main Box */}
-            <div className="login-main-box">
-                {/* Gold Box */}
-                <div className="login-gold-box"></div>
+        <div className="login-main-box">
+            {/* Gold Box */}
+            <div className="login-gold-box"></div>
 
-                {/* Poly Planner */}
-                <div className="login-poly-planner">Poly Planner</div>
+            {/* Poly Planner */}
+            <div className="login-poly-planner">Poly Planner</div>
 
-                {/* Username */}
-                <div className="login-username">Username</div>
-                <input
-                    className="login-username-box"
-                    type="text"
-                    name="username"
-                    value={creds.username}
-                    onChange={handleChange}
-                    style={{ fontSize: "18px" }}
-                />
+            {/* Username */}
+            <div className="login-username">Username</div>
+            <input
+                className="login-username-box"
+                type="text"
+                name="username"
+                value={creds.username}
+                onChange={handleChange}
+                style={{ fontSize: "18px" }}
+            />
 
-                {/* Password */}
-                <div className="login-password">Password</div>
-                <input
-                    className="login-password-box"
-                    type="password"
-                    name="password"
-                    value={creds.pwd}
-                    onChange={handleChange}
-                    style={{ fontSize: "18px" }}
-                />
+            {/* Password */}
+            <div className="login-password">Password</div>
+            <input
+                className="login-password-box"
+                type="password"
+                name="password"
+                value={creds.pwd}
+                onChange={handleChange}
+                style={{ fontSize: "18px" }}
+            />
 
-                {/* Error Message */}
-                {props.message && (
-                    <div className="login-error-message">{props.message}</div>
-                )}
+            {/* Error Message */}
+            {props.message && (
+                <div className="login-error-message">{props.message}</div>
+            )}
 
-                {/* Login Box */}
-                <button className="login-login-box" onClick={submitForm}>
-                    <p className="login-login">Login</p>
-                </button>
+            {/* Login Box */}
+            <button className="login-login-box" onClick={submitForm}>
+                <span className="login-login">Login</span>
+            </button>
 
-                {/* Register Button */}
-                <button
-                    className="login-register-button"
-                    onClick={handleRegister}
-                >
-                    <p className="login-register-text">Create Account</p>
-                </button>
-            </div>
+            {/* Register Button */}
+            <button
+                className="login-register-button"
+                onClick={handleRegister}
+            >
+                <p className="login-register-text">Create Account</p>
+            </button>
         </div>
     );
 }
