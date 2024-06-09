8b805a1e6f62af173a50331cb2ef45f776cc5673
klseverson <113937747+klseverson@users.noreply.github.com>
2024-06-08 20:57:45 -0700

    config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,35 +9,3 @@ export default [
     pluginJs.configs.recommended,
     pluginReactConfig
 ];
-
-/*import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
-export default [
-  {
-    languageOptions: {
-      globals: globals.browser,
-      ecmaVersion: 2021,
-      sourceType: "module",
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    plugins: {
-      react: pluginReact,
-    },
-    rules: {
-      ...pluginJs.configs.recommended.rules,
-      ...pluginReact.configs.recommended.rules,
-    },
-    overrides: [
-      {
-        files: ['src/**\/*.{js,jsx}'],
-      },
-    ],
-  },
-];
- */
